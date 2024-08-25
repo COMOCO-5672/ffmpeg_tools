6d8fcd101c3e98d30124b938d5ee1ca4ce099af3
comoco-5672 <775324623@qq.com>
2024-08-25 15:48:01 +0800

    添加对HDR/SDR性能检测功能

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,119 @@ std::vector<EncoderInfo> detect_hardware_encoders() {
   return encoders;
 }
 
+// support SDR or HDR
+double test_encoder_performance(const EncoderInfo &encoder, bool is_hdr) {
+  AVCodec *codec = avcodec_find_encoder_by_name(encoder.name.c_str());
+  if (!codec)
+    return 0.0;
+
+  AVCodecContext *c = avcodec_alloc_context3(codec);
+  if (!c)
+    return 0.0;
+
+  c->bit_rate = 5000000;
+  c->width = TEST_WIDTH;
+  c->height = TEST_HEIGHT;
+  c->time_base = (AVRational){1, 25};
+  c->framerate = (AVRational){25, 1};
+  c->gop_size = 10;
+  c->max_b_frames = 1;
+  c->pix_fmt = is_hdr ? AV_PIX_FMT_YUV420P10LE : AV_PIX_FMT_YUV420P;
+
+  if (is_hdr) {
+    c->color_primaries = AVCOL_PRI_BT2020;
+    c->color_trc = AVCOL_TRC_SMPTE2084;
+    c->colorspace = AVCOL_SPC_BT2020_NCL;
+  } else {
+    c->color_primaries = AVCOL_PRI_BT709;
+    c->color_trc = AVCOL_TRC_BT709;
+    c->colorspace = AVCOL_SPC_BT709;
+  }
+
+  if (avcodec_open2(c, codec, NULL) < 0) {
+    avcodec_free_context(&c);
+    return 0.0;
+  }
+
+  AVFrame *frame = av_frame_alloc();
+  if (!frame) {
+    avcodec_free_context(&c);
+    return 0.0;
+  }
+
+  frame->format = c->pix_fmt;
+  frame->width = c->width;
+  frame->height = c->height;
+  frame->color_primaries = c->color_primaries;
+  frame->color_trc = c->color_trc;
+  frame->colorspace = c->colorspace;
+  av_frame_get_buffer(frame, 0);
+
+  AVPacket *pkt = av_packet_alloc();
+  if (!pkt) {
+    av_frame_free(&frame);
+    avcodec_free_context(&c);
+    return 0.0;
+  }
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  for (int i = 0; i < TEST_FRAMES; i++) {
+    int max_value = is_hdr ? 1023 : 255;
+    for (int y = 0; y < c->height; y++) {
+      for (int x = 0; x < c->width; x++) {
+        if (is_hdr) {
+          ((uint16_t *)frame->data[0])[y * frame->linesize[0] / 2 + x] =
+              ((x + y + i * 3) * 4) & max_value;
+        } else {
+          frame->data[0][y * frame->linesize[0] + x] =
+              (x + y + i * 3) & max_value;
+        }
+      }
+    }
+    for (int y = 0; y < c->height / 2; y++) {
+      for (int x = 0; x < c->width / 2; x++) {
+        if (is_hdr) {
+          ((uint16_t *)frame->data[1])[y * frame->linesize[1] / 2 + x] =
+              ((512 + y + i * 2) * 4) & max_value;
+          ((uint16_t *)frame->data[2])[y * frame->linesize[2] / 2 + x] =
+              ((256 + x + i * 5) * 4) & max_value;
+        } else {
+          frame->data[1][y * frame->linesize[1] + x] =
+              (128 + y + i * 2) & max_value;
+          frame->data[2][y * frame->linesize[2] + x] =
+              (64 + x + i * 5) & max_value;
+        }
+      }
+    }
+
+    frame->pts = i;
+
+    int ret = avcodec_send_frame(c, frame);
+    if (ret < 0)
+      break;
+
+    while (ret >= 0) {
+      ret = avcodec_receive_packet(c, pkt);
+      if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+        break;
+      if (ret < 0)
+        break;
+      av_packet_unref(pkt);
+    }
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> diff = end - start;
+
+end:
+  av_frame_free(&frame);
+  av_packet_free(&pkt);
+  avcodec_free_context(&c);
+
+  return TEST_FRAMES / diff.count();
+}
+
 double test_encoder_performance(const EncoderInfo &encoder) {
   AVCodec *codec = avcodec_find_encoder_by_name(encoder.name.c_str());
   if (!codec) {
@@ -139,6 +252,8 @@ EncoderInfo find_best_encoder() {
     std::cout << "Testing encoder:" << encoder.name << " ("
               << av_hwdevice_get_type_name(encoder.hw_type) << ")..."
               << std::endl;
+    // Encode test video with the best encoder,
+    // "test_encoder_performance(encoder, is_hdr)" support HDR or SDR
     encoder.performance = test_encoder_performance(encoder);
     std::cout << "Performance: " << encoder.performance << " fps" << std::endl;
   }
